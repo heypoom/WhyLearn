b71c2c6ac65467d4f1beed0e77235cd591b42b24
Phoomparin Mano <work@phoom.in.th>
2017-10-16 11:27:42 +0700

    :art: Initialized Emotion

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-export default () => (
+import styled from 'react-emotion'
+
+const Heading = styled.h1`
+  font-family: 'Sukhumvit Set';
+`
+
+const Landing = () => (
   <div>
-    Hello, Next!
+    <Heading>รักสมศักดิ์ เจียมธีระสกุล</Heading>
   </div>
 )
+
+export default Landing
+
